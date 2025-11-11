e60f5f45257adf34d65eda4a85834c6d764ed3f2
Marcus <mdevhurt@gmail.com>
2025-11-11 01:08:43 +0000

    Added doxygen comments to the listener.

--- include/beginner_tutorials/listener.hpp
+++ include/beginner_tutorials/listener.hpp
@@ -1,4 +1,16 @@
-
+/**
+ * @file listener.hpp
+ * @author Marcus Hurt (mhurt@umd.edu)
+ * @brief This is a small listener node that subscribes to the "/topic" topic
+ * and logs received messages.
+ * @version 0.1
+ * @date 2025-11-11
+ * @details listener
+ * See the LICENSE file for license information.
+ *
+ * @copyright Copyright (c) 2025 Marcus Hurt
+ *
+ */
 
 #pragma once
 
@@ -9,11 +21,20 @@
 #include "std_msgs/msg/string.hpp"
 
 class MyListener : public rclcpp::Node {
-public:
-    MyListener();
+ public:
+  /**
+   * @brief Construct a new My Listener object
+   *
+   */
+  MyListener();
 
-private:
-    void topic_callback(const std_msgs::msg::String& msg) const;
+ private:
+  /**
+   * @brief Callback function for the subscription
+   *
+   * @param msg - the received message
+   */
+  void topic_callback(const std_msgs::msg::String& msg) const;
 
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
--- src/listener_node.cpp
+++ src/listener_node.cpp
@@ -1,6 +1,17 @@
-
-#include "rclcpp/rclcpp.hpp"
+/**
+ * @file listener_node.cpp
+ * @author Marcus Hurt (mhurt@umd.edu)
+ * @brief Minimal driver for the listener node.
+ * @version 0.1
+ * @date 2025-11-11
+ * @details listener_node
+ * See the LICENSE file for license information.
+ *
+ * @copyright Copyright (c) 2025 Marcus Hurt
+ * 
+ */
 #include "beginner_tutorials/listener.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
